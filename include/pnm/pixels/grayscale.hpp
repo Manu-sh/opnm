300fb2f11d2ddef1fc9d7c1bfb4b280ccf7a2f79
Emanuele Innamorati <s3gmentationfault@gmail.com>
2025-11-19 23:23:02 +0100

    Fix attribute syntax for packed grayscale struct

--- include/pnm/pixels/grayscale.hpp
+++ include/pnm/pixels/grayscale.hpp
@@ -9,7 +9,7 @@ namespace pnm {
     template <const uint8_t BIT_DEPTH> struct grayscale;
 
     template <>
-    struct __attribute__((__packed__)) grayscale<BIT_8> {
+    struct __attribute__((packed)) grayscale<BIT_8> {
         grayscale() = default;
         inline constexpr grayscale(uint8_t r, uint8_t g, uint8_t b): data(.3 * r + .59 * g + .11 * b) {} // convert from rgb to grayscale using the luminosity method
         inline constexpr grayscale(const rgb<BIT_8> &px): grayscale{px.r, px.g, px.b} {}
